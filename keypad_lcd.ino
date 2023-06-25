aaf1ddc2f54d7f6a81f7a1cda6c3151928956732
edgarchakryan <32942594+edgarchakryan@users.noreply.github.com>
2023-06-25 04:05:50 -0700

    keypad_lcd.ino

--- keypad_lcd.ino
+++ keypad_lcd.ino
@@ -3,20 +3,20 @@
 
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
-const byte ROWS = 4;
-const byte COLS = 4;
+const byte rows = 4;
+const byte cols = 4;
 
-char hexaKeys[ROWS][COLS] = {
+char hexaKeys[rows][cols] = {
   {'1','2','3','A'},
   {'4','5','6','B'},
   {'7','8','9','C'},
   {'*','0','#','D'}
 };
 
-byte rowPins[ROWS] = {22, 24, 26, 28}; 
-byte colPins[COLS] = {30, 32, 34, 36};
+byte rowPins[rows] = {22, 24, 26, 28}; 
+byte colPins[cols] = {30, 32, 34, 36};
 
-Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, rows, cols); 
 
 void setup()
 {
